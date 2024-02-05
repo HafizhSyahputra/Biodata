0fa64d52932f05a66a96a4403079228673880c6b
Xyne <hafizhsyaputra676@gmail.com>
2024-02-05 15:56:43 +0700

    new

--- app/src/main/java/com/example/biodata/MainActivity.java
+++ app/src/main/java/com/example/biodata/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.biodata;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,43 +11,89 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
 
-    private EditText etNama, etAlamat;
-    private Spinner listItem;
-    private RadioGroup pilihJK;
-    private Button submitButton;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editTextNis;
+    private EditText editTextnama;
+    private EditText editTextalamat;
+    private Spinner editTextkota;
+    private RadioGroup radioGroupJK;
+    private EditText editTextumur;
+    private Button buttonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNama = findViewById(R.id.ETnama);
-        etAlamat = findViewById(R.id.ETalamat);
-        listItem = findViewById(R.id.listItem);
-        pilihJK = findViewById(R.id.pilih_jk);
-        submitButton = findViewById(R.id.submitButton);
+        editTextNis = findViewById(R.id.ETnis);
+        editTextnama = findViewById(R.id.ETnama);
+        editTextalamat = findViewById(R.id.ETalamat);
+        editTextkota = findViewById(R.id.listItem);
+        radioGroupJK = findViewById(R.id.pilih_jk);
+        editTextumur = findViewById(R.id.ETUmur);
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nama = etNama.getText().toString();
-                String alamat = etAlamat.getText().toString();
-                String kota = listItem.getSelectedItem().toString();
-                RadioButton selectedRadioButton = findViewById(pilihJK.getCheckedRadioButtonId());
-                String jenisKelamin = selectedRadioButton.getText().toString();
+        buttonAdd = findViewById(R.id.submitButton);
+        buttonAdd.setOnClickListener(this);
+    }
 
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+    private void addEmployee() {
+        final String nis = editTextNis.getText().toString().trim();
+        final String nama = editTextnama.getText().toString().trim();
+        final String alamat = editTextalamat.getText().toString().trim();
+        final String kota = editTextkota.getSelectedItem().toString().trim();
 
-                intent.putExtra("NAMA", nama);
-                intent.putExtra("ALAMAT", alamat);
-                intent.putExtra("KOTA", kota);
-                intent.putExtra("JENIS_KELAMIN", jenisKelamin);
-                startActivity(intent);
+        int selectedId = radioGroupJK.getCheckedRadioButtonId();
+        RadioButton selectedRadioButton = findViewById(selectedId);
 
+        if (selectedRadioButton != null) {
+            final String jk = selectedRadioButton.getText().toString().trim();
+            final String umur = editTextumur.getText().toString().trim();
+
+            class AddEmployee extends AsyncTask<Void, Void, String> {
+                ProgressDialog loading;
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    loading = ProgressDialog.show(MainActivity.this, "Menambahkan...", "Tunggu...", false, false);
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    loading.dismiss();
+                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                protected String doInBackground(Void... v) {
+                    HashMap<String, String> params = new HashMap<>();
+                    params.put(com.example.biodata.konfigurasi.KEY_EMP_NIS, nis);
+                    params.put(com.example.biodata.konfigurasi.KEY_EMP_NAMA, nama);
+                    params.put(com.example.biodata.konfigurasi.KEY_EMP_ALAMAT, alamat);
+                    params.put(com.example.biodata.konfigurasi.KEY_EMP_KOTA, kota);
+                    params.put(com.example.biodata.konfigurasi.KEY_EMP_JK, jk);
+                    params.put(com.example.biodata.konfigurasi.KEY_EMP_UMUR, umur);
+                    RequestHandler rh = new RequestHandler();
+                    return rh.sendPostRequest(com.example.biodata.konfigurasi.URL_ADD, params);
+                }
             }
-        });
+
+            AddEmployee ae = new AddEmployee();
+            ae.execute();
+        } else {
+            Toast.makeText(MainActivity.this, "Pilih jenis kelamin", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonAdd) {
+            addEmployee();
+        }
     }
 }
---
+++ app/src/main/java/com/example/biodata/konfigurasi.java
@@ -0,0 +1,24 @@
+package com.example.biodata;
+
+public class konfigurasi {
+
+    public static final String KEY_EMP_NIS = "nis";
+    public static final String KEY_EMP_NAMA = "nama";
+    public static final String KEY_EMP_ALAMAT = "alamat";
+    public static final String KEY_EMP_KOTA = "kota";
+    public static final String KEY_EMP_JK = "jk";
+    public static final String KEY_EMP_UMUR = "umur";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_NIS = "nis";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_ALAMAT = "alamat";
+    public static final String TAG_KOTA = "kota";
+    public static final String TAG_JK = "jk";
+    public static final String TAG_UMUR = "umur";
+
+    public static final  String URL_ADD="http://10.107.76.5/android/Tambahdata.php";
+
+    public static final String EMP_ID = "emp_id";
+}
